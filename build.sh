72567c918b10f9586a4d1ce613f34d893b24821d
Yahya Jabary <jabaryyahya@gmail.com>
2024-03-13 23:06:28 +0100

    up

--- build.sh
+++ build.sh
@@ -10,30 +10,55 @@ if [ "$(id -u)" != "0" ]; then
     return
 fi
 
-# ----------------------------------------------------------------------------------------------- delete everything
+# ----------------------------------------------------------------------------------------------- create superuser account
 # create superuser account
 echo "${green}creating superuser account${reset}"
 username="postgres"
-psql postgres -c "CREATE USER $username SUPERUSER"
-sudo -u postgres psql
+password="password"
+psql postgres -c "CREATE USER $username SUPERUSER PASSWORD '$password';"
 
-# delete all databases
-echo "${green}deleting all databases${reset}"
-databases=($(psql postgres -t -c "SELECT datname FROM pg_database WHERE datistemplate = false;"))
+# ----------------------------------------------------------------------------------------------- delete everything
+getUsers() {
+    psql postgres -t -c "SELECT rolname FROM pg_roles WHERE rolcanlogin = true;"
+}
+getTables() {
+    psql $1 -t -c "SELECT tablename FROM pg_tables WHERE schemaname = 'public';"
+}
+getDatabases() {
+    psql postgres -t -c "SELECT datname FROM pg_database WHERE datistemplate = false;"
+}
+
+# delete tables
+echo "${green}deleting tables${reset}"
+databases=($(getDatabases))
 for database in "${databases[@]}"; do
-    if [ "$database" == "postgres" ]; then
+    tables=($(getTables $database))
+    for table in "${tables[@]}"; do
+        echo "deleting table: $table"
+        psql $database -c "DROP TABLE IF EXISTS $table CASCADE;"
+    done
+done
+echo "tables left:"
+psql postgres -c "\dt"
+
+# delete databases
+echo "${green}deleting databases${reset}"
+databases=($(getDatabases))
+for database in "${databases[@]}"; do
+    if [ "$database" = "postgres" ]; then
         echo "skipping database: $database"
         continue
     fi
+
     echo "deleting database: $database"
-    sudo -u postgres psql -c "DROP DATABASE $database;"
+    psql postgres -c "DROP DATABASE IF EXISTS $database"
 done
 echo "databases left:"
 psql postgres -c "\l"
 
-# delete each user
-echo "${green}deleting all users${reset}"
-users=($(psql postgres -t -c "SELECT rolname FROM pg_roles WHERE rolcanlogin = true;"))
+# iterate through users
+echo "${green}deleting users${reset}"
+users=($(getUsers))
 for user in "${users[@]}"; do
     echo "deleting user: $user"
     sudo -u postgres psql -c "DROP ROLE $user;"
@@ -41,7 +66,7 @@ done
 echo "users left:"
 psql postgres -c "\du"
 
-# ----------------------------------------------------------------------------------------------- generate user and database
+# ----------------------------------------------------------------------------------------------- create new user and database
 # create user account
 echo "${green}creating user account${reset}"
 username=$(whoami)
@@ -49,35 +74,18 @@ password="password"
 psql postgres -c "CREATE ROLE $username WITH LOGIN PASSWORD '$password';"
 psql postgres -c "ALTER ROLE $username CREATEDB;"
 
-# create database
-echo "${green}creating database${reset}"
+# delete database if it exists
 database="testdb"
-psql postgres -c "CREATE DATABASE $database WITH OWNER $username;"
-psql $database -c "CREATE SCHEMA public;"
-psql $database -c "GRANT ALL PRIVILEGES ON SCHEMA public TO $username;"
-psql $database -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $username;"
-psql $database -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO $username;"
-psql $database -c "GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO $username;"
-psql $database -c "GRANT ALL PRIVILEGES ON DATABASE $database TO $username;"
-psql $database -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO public;"
-psql $database -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO public;"
-psql $database -c "GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO public;"
-psql $database -c "GRANT ALL PRIVILEGES ON DATABASE $database TO public;"
-psql $database -c "GRANT ALL PRIVILEGES ON SCHEMA public TO public;"
-psql $database -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $username;"
-psql $database -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO $username;"
+# psql postgres -c "DROP DATABASE IF EXISTS $database;"
+
+# create database
+# echo "${green}creating database${reset}"
+# psql postgres -c "CREATE DATABASE $database WITH OWNER $username;"
+# psql $database -c "GRANT ALL PRIVILEGES ON DATABASE $database TO $username;"
+# psql $database -c "CREATE SCHEMA public;"
+# psql $database -c "GRANT ALL PRIVILEGES ON DATABASE $database TO public;"
 
 # ----------------------------------------------------------------------------------------------- run sql scripts
-# delete all tables
-echo "${green}deleting all tables${reset}"
-tables=($(psql $database -t -c "SELECT tablename FROM pg_tables WHERE schemaname = 'public';"))
-for table in "${tables[@]}"; do
-    echo "deleting table: $table"
-    psql $database -c "DROP TABLE $table;"
-done
-echo "tables left:"
-psql $database -c "\dt"
-
 # run all sql scripts
 echo "${green}running sql scripts${reset}"
 path="./scripts/*.sql"
